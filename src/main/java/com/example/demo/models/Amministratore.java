845648de2d1bc18e4dbff6e37568523df9642ef2
SimoneBarbella <simo.barbella1@gmail.com>
2025-12-02 18:59:53 +0100

    Add files via upload

---
+++ src/main/java/com/example/demo/models/Amministratore.java
@@ -0,0 +1,50 @@
+package com.example.demo3.models;
+
+public class Amministratore extends Utente {
+	private String partitaIva;
+	private String nomeAgenziaImmobiliare;
+	
+	
+	
+	public Amministratore() {
+		super();
+	}
+	
+	
+
+
+	public Amministratore(String nome, String cognome, String email, String password) {
+		super(nome, cognome, email, password);
+		setPartitaIva( partitaIva);
+		setNomeAgenziaImmobiliare(nomeAgenziaImmobiliare);
+		}
+
+
+
+
+	public String getPartitaIva() {
+		return partitaIva;
+	}
+
+
+
+	public void setPartitaIva(String partitaIva) {
+		this.partitaIva = partitaIva;
+	}
+
+
+
+	public String getNomeAgenziaImmobiliare() {
+		return nomeAgenziaImmobiliare;
+	}
+
+
+
+	public void setNomeAgenziaImmobiliare(String nomeAgenziaImmobiliare) {
+		this.nomeAgenziaImmobiliare = nomeAgenziaImmobiliare;
+	}
+	
+	
+	
+	
+}
---
+++ src/main/java/com/example/demo/models/Cliente.java
@@ -0,0 +1,15 @@
+package com.example.demo3.models;
+
+public class Cliente extends Utente{
+
+	public Cliente() {
+		super();
+	}
+	
+	
+	public Cliente(String nome, String cognome, String email, String password) {
+		super(nome, cognome, email, password);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
