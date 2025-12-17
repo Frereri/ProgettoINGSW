703352d863b546b24bcfe4ac6d3729bf5177fc34
SimoneBarbella <simo.barbella1@gmail.com>
2025-12-17 18:17:42 +0100

    Add files via upload

--- src/main/java/com/example/demo/models/Cliente.java
+++ src/main/java/com/example/demo/models/Cliente.java
@@ -1,15 +1,47 @@
 package com.example.demo3.models;
 
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name="cliente", schema="dietiestes")
 public class Cliente extends Utente{
 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+	private Integer idCliente;
+	
+
+	
 	public Cliente() {
 		super();
 	}
 	
 	
-	public Cliente(String nome, String cognome, String email, String password) {
-		super(nome, cognome, email, password);
-		// TODO Auto-generated constructor stub
+	public Cliente(Integer idCliente, Agente agenteDiRiferimento) {
+		super();
+		this.idCliente = idCliente;
+
+	}
+
+
+	public Integer getIdCliente() {
+		return idCliente;
+	}
+
+
+	public void setIdCliente(Integer idCliente) {
+		this.idCliente = idCliente;
 	}
 	
 }
